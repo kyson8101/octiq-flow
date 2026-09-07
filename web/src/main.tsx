906ab31971a081cd4e90e37c6c079eb5254a4ee7
Kyson Teh <ksteh@pandaworks.net>
2026-09-07 13:37:20 +0800

    feat(ui): use IBM Plex Sans for interface and prose

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,8 @@ import { lazy, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { ConfirmProvider } from "./components/Confirm";
 import { OpenFileProvider } from "./components/OpenFile";
+import "@fontsource-variable/ibm-plex-sans/wght.css";
+import "@fontsource-variable/ibm-plex-sans/wght-italic.css";
 import "./styles.css";
 import { applyTheme, savedThemeId } from "./lib/themeStore";
 
